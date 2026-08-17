85e6714cf0e57e362e04f4ee5f495c30545b997f
CSMahar <chandanmahar777@gmail.com>
2026-08-17 13:40:17 +0530

    remove localhost call

--- api/index.py
+++ api/index.py
@@ -29,6 +29,6 @@ def show_project(id):
         return "Not Found", 404
     return render_template("projectpage.html",project=project)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=True)
 
